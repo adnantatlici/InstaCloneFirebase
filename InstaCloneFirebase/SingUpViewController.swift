ec19abe746573049457c0ff95b0d1e7dc94a6da5
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-17 19:10:40 +0300

    first commit

---
+++ InstaCloneFirebase/SingUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SingUpViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Mustafa Adnan Tatlıcı on 17.01.2023.
+//
+
+import UIKit
+
+class SingUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
