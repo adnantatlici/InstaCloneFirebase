caae877c43be6822c35a5e683db9613d975d41a3
Mustafa Adnan Tatlici <m.adnantatlici@gmail.com>
2023-01-17 19:18:22 +0300

    First

--- InstaCloneFirebase/SingUpViewController.swift
+++ InstaCloneFirebase/SingUpViewController.swift
@@ -6,24 +6,52 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SingUpViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
     
+    
+    
+    @IBAction func signUpClicked(_ sender: Any) {
 
-    /*
-    // MARK: - Navigation
+        if emailText.text != "" && passwordText.text != "" {
+  
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { authdata, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error" )
+                } else {
+                    self.performSegue(withIdentifier: "toSignInVC", sender: nil)
+                }
+                }
+        } else {
+            makeAlert(titleInput: "error", messageInput: "username/pass")
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
     }
-    */
-
+    
+    func makeAlert(titleInput: String, messageInput:String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
